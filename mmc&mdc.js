869da5d90e14043e2e27f57ae4a804e161867a5c
Thiago de Oliveira Sopeña Alves <129515222+ThiagoOSA@users.noreply.github.com>
2025-08-31 04:31:31 +0000

    Add files via upload

---
+++ mmc&mdc.js
@@ -0,0 +1,26 @@
+function mmc(...values){
+    const val = values.length ? values.flat(Infinity) : this;
+    if(!val.isNaN || val.some((v) => v%1 || isNaN(v))){
+        return "Insira apenas numeros validos";
+    }
+    for(let max = Math.max(...val), i = max; !val.every((v)=>i%v === 0); i+=max );
+    return i;
+}
+
+function mdc(...values){
+    const val = values.length ? values.flat(Infinity) : this;
+    if(!val.length || val.some((v) => v%1 || isNaN(v))){
+        return "Insira apenas numeros validos";
+    }
+    val.sort((a,b)=>b-a);
+    let i = val.reduce((a,v)=>{
+        while(v){
+            [a,v] = [v, a%v];
+        }
+        return a;
+    })
+    return i;
+}
+
+Array.prototype.mmc = mmc;
+Array.prototype.mdc = mdc;
